ea3fa86e6e4395f2a9b25f22c9e765c26ad4856a
ZimdAustria <45945462+ZimdAustria@users.noreply.github.com>
2019-02-14 13:36:52 +0100

    updated main.js
    source: example from
    https://medium.com/@loginov_rocks/how-to-make-a-web-app-for-your-own-bluetooth-low-energy-device-arduino-2af8d16fdbe8

--- main.js
+++ main.js
@@ -5,4 +5,99 @@ let terminalContainer = document.getElementById('terminal');
 let sendForm = document.getElementById('send-form');
 let inputField = document.getElementById('input');
 
+//connect the device on connect button click
+connectButton.addEventListener('click',function(){
+                               connect();
+                               });
 
+//Disconnect from the device on disconnect button click
+disconnectButton.addEventListener('click',function(){
+                                  disconnect();
+                                  });
+
+//Handle form submit event
+sendForm.addEventListener('submit',function(event){
+                          event.preventDefault();   //Prevent from sending
+                          send(inputField.value);   //send text field content
+                          inputField.value = '';    //zero text field
+                          inputField.focus();       //focus on text field
+                          });
+
+//Selected device object cache
+let deviceCache = null;
+
+//Launch Bluetooth device chooser and connect to the selected device
+function connect(){
+    return (deviceCache ? Promise.resolve(deviceCache) :
+        requestBluetoothDevice()).
+        then(device => connectDeviceAndCacheCharacteristic(device)).
+        then(characteristic => startNotifications(characteristic)).
+        catch(error => log(error));
+}
+
+function requestBluetoothDevice(){
+    log('Requesting Bluetooth device...');
+    
+    return navigator.bluetooth.requestDevice({
+                                             filters: [{services: [0xFFE0]}],
+    }).
+    then(device => {
+         log('"' + device.name + '" bluetooth device selected');
+         deviceCache = device;
+         
+         return deviceCache;
+         });
+}
+
+//Characteristic object cache
+let characteristicCache = null;
+
+//Connect to speccified device, get service and characteristic
+function connectDeviceAndCacheCharacteristic(device){
+    if(device.gatt.connected && characteristicCache) {
+        return Promise.resolve(characteristicCache);
+    }
+    
+    log('Connecting to GATT Server...');
+    
+    return device.gatt.connect().
+        then(server => {
+             log('GATT server connected, getting service...');
+             
+             return server.getPrimaryService(0xFFE0);
+        }).
+        then(service => {
+             log('Service found, getting characteristic...');
+             return service.getCharacteristic(0xFFE1);
+        }).
+        then(characteristic => {
+             log('Characteristic found');
+             characteristicCache = characteristic;
+             
+             return characteristicCache;
+        });
+}
+
+function startNotifications(characteristic){
+    log('Starting notifications...');
+    
+    return characteristic.startNotifications().
+        then(() => {
+         log('Notifications started');
+        });
+}
+
+//Output to terminal
+function log(data, type=''){
+    terminalContainer.insertAdjacentHTML('beforeend', '<div' + (type ? 'class="' + type + '"' : '') + '>' + data + '</div>');
+}
+
+//Disconnect from device
+function disconnect(){
+    //
+}
+
+//Send data to the connected device
+function send(data){
+    //
+}
