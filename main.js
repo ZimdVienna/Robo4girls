6c7950fec24d1d5da1decc5fd780798a727ac574
ZimdAustria <45945462+ZimdAustria@users.noreply.github.com>
2019-02-14 12:57:47 +0100

    web app test files upload
    source: https://medium.com/@loginov_rocks/how-to-make-a-web-app-for-your-own-bluetooth-low-energy-device-arduino-2af8d16fdbe8
    by Danila Loginov

---
+++ main.js
@@ -0,0 +1,8 @@
+// Get references to UI elements
+let connectButton = document.getElementById('connect');
+let disconnectButton = document.getElementById('disconnect');
+let terminalContainer = document.getElementById('terminal');
+let sendForm = document.getElementById('send-form');
+let inputField = document.getElementById('input');
+
+
