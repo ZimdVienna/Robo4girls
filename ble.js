50b74cab4b05279dd9a6e6622057ea5879d9c5c8
Daniela Riedl <Makki@Danielas-MacBook-Pro-3.local>
2019-02-20 15:13:25 +0100

    Revert "Update ble.js"
    This reverts commit 94d1555315485902c09e89ebc719b2b07bb45781.

--- ble.js
+++ ble.js
@@ -44,7 +44,7 @@ function requestBluetoothDevice(){
     return navigator.bluetooth.requestDevice({
                                              filters: [{
                                                        "namePrefix" : name_prefix
-                                                       }],
+                                                       }]
                                              optionalServices: [service_uart]          
                                              })
     .then(device => {
