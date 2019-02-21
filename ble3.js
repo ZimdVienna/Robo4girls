e9df7c4b542c121819342010494fbadbe4e969c0
ZimdAustria <45945462+ZimdAustria@users.noreply.github.com>
2019-02-21 14:01:24 +0100

    ble connect functional
    works:
    + scan for and connect to device
    + find service and tx characteristic
    + start notifications for tx characteristic (get messages from connected device)

--- ble3.js
+++ ble3.js
@@ -8,13 +8,13 @@ const inputField = document.getElementById("input");
 
 const name_prefix = "BBC micro:bit";
 const uart_service = '6e400001-b5a3-f393-e0a9-e50e24dcca9e';
-//const uart_characteristic_tx = '6e400002-b5a3-f393-e0a9-e50e24dcca9e';
-const uart_characteristic_tx = 0x0002;
-const uart_characteristic_rx = '6e400003-b5a3-f393-e0a9-e50e24dcca9e';
+const uart_characteristic_tx = '6e400002-b5a3-f393-e0a9-e50e24dcca9e';  //messages from micro:bit
+const uart_characteristic_rx = '6e400003-b5a3-f393-e0a9-e50e24dcca9e';  //messages to the micro:bit
 
-let deviceCache = null;
-let characteristicCache_tx = null;
-let characteristicCache_rx = null;
+//save the device for reconnection
+var deviceCache = null;
+var characteristicCache_tx = null;
+var characteristicCache_rx = null;
 
 //connect to device on button click
 connectButton.addEventListener('click', function(){
@@ -32,64 +32,14 @@ reconnectButton.addEventListener('click', function(){
 })
 
 
-/* FUNCTIONS */
+/* ******************* FUNCTIONS ******************* */
 
 // Output to terminal
 function log(data, type = '') {
     terminalContainer.insertAdjacentHTML('beforeend','<div' + (type ? ' class="' + type + '"' : '') + '>' + data + '</div>');
 }
 
-//request Bluetooth device with name prefix "BBC micro:bit"
-function requestBluetoothDevice() {
-    log("Requesting Bluetooth device...");
-    return navigator.bluetooth.requestDevice({
-        filters: [{ "name_prefix" : name_prefix }],
-        optionalServices: [uart_service]
-    })
-    .then(device => {
-        deviceCache = device;
-        deviceCache.addEventListener('gattserverdisconnected', onDisconnected); // watch connection
-        log('"' + deviceCache.name);
-        return deviceCache;
-    })
-    .catch(error => {
-        log(error);
-    });
-}
-
-function connectDeviceAndCacheCharacteristic(device){
-    // if already connected
-    if(device.gatt.connected && characteristicCache_tx) {
-        return Promise.resolve(characteristicCache_tx);
-    }
-    
-    log("Connecting to GATT Server...");
-    return device.gatt.connect()
-    .then(server => {
-        log("GATT Server connected, getting service...");
-        return server.getPrimaryService(uart_service);
-    })
-    .then(service => {
-        log("Service found, get rx characteristic...");
-        return service.getCharacteristic(uart_characteristic_rx);
-    })
-    .then(characteristic => {
-        log("Characteristic found");
-        characteristicCache_rx = characteristic;
-        return characteristicCache_rx;
-    })
-    .catch(error => {
-        log(error);
-    });
-}
-
-//checks for connection loss
-function onDisconnected(event) {
-  // Object event.target is Bluetooth Device getting disconnected.
-  log("Bluetooth Device disconnected");
-}
-
-/* button functions */
+/* BUTTON FUNCTIONS */
 
 //disconnect
 function onDisconnectButtonClick(){
@@ -123,11 +73,78 @@ function onReconnectButtonClick(){
 //connect
 function onConnectButtonClick() {
     return (deviceCache ? Promise.resolve(deviceCache) : requestBluetoothDevice())
-    .then(device => connectDeviceAndCacheCharacteristic(deviceCache))
-    //.then(characteristic => startNotifications(characteristic))
+    .then(device => connectDeviceAndCacheCharacteristic(device))
+    .then(characteristic => startNotifications(characteristic))
     .catch(error => {
         log(error);
     });
 }
+
+/* BLE FUNCTIONS */
+
+//checks for connection loss
+function onDisconnected(event) {
+  // Object event.target is Bluetooth Device getting disconnected.
+  log("Bluetooth Device disconnected");
+}
+
+//request Bluetooth device with name prefix "BBC micro:bit
+function requestBluetoothDevice() {
+    
+    let options = {
+    filters: [
+        {namePrefix: name_prefix}
+    ],
+    optionalServices: [uart_service] 
+    }
+    log("Requesting Bluetooth device...");
+    
+    return navigator.bluetooth.requestDevice(options)
+    .then(device => {
+        deviceCache = device;
+        deviceCache.addEventListener('gattserverdisconnected', onDisconnected); // watch connection
+        log('"' + deviceCache.name + '" bluetooth device selected');
+        return deviceCache;
+    })
+    .catch(error => {
+        log(error);
+    });
+}
+
+function connectDeviceAndCacheCharacteristic(device){
+    // if already connected
+    if(device.gatt.connected && characteristicCache_tx) {
+        return Promise.resolve(characteristicCache_tx);
+    }
+    
+    log("Connecting to GATT Server...");
+    return device.gatt.connect()
+    .then(server => {
+        log("GATT Server connected, getting service...");
+        return server.getPrimaryService(uart_service);
+    })
+    .then(service => {
+        log("Service found, get rx characteristic...");
+        return service.getCharacteristic(uart_characteristic_tx);
+    })
+    .then(characteristic => {
+        log("Characteristic found");
+        characteristicCache_tx = characteristic;
+        return characteristicCache_tx;
+    })
+    .catch(error => {
+        log(error);
+    });
+}
+
+function startNotifications(characteristic){
+    log('Starting notifications...');
+    
+    return characteristic.startNotifications()
+    .then(() => {
+         log('Notifications started');
+    });
+}
+
 
 
