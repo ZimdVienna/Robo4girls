2f3c9d34c6b582c5d56017e01beb5434e68f946f
Daniela Riedl <Makki@Danielas-MacBook-Pro-3.local>
2019-04-18 11:52:11 +0200

    added melodies
    + don't Worry be Happy
    + Somewhere over the Rainbow

--- scripts/R4Gblocks.js
+++ scripts/R4Gblocks.js
@@ -290,7 +290,7 @@ Blockly.Blocks['melody'] = {
 	init: function () {
 		this.appendDummyInput()
 			.appendField("Melodie abspielen")
-			.appendField(new Blockly.FieldDropdown([["Tusch", "M1"], ["Romantisch", "M2"], ["Star Wars", "M3"],["Super Mario", "M4"],["Donauwalzer", "M5"],["Tango", "M6"]]), "melody");
+			.appendField(new Blockly.FieldDropdown([["Tusch", "M1"], ["Romantisch", "M2"], ["Star Wars", "M3"],["Super Mario", "M4"],["Donauwalzer", "M5"],["Tango Kriminalis", "M6"],["Don't Worry be Happy", "M7"],["Somewhere over the Rainbow", "M8"]]), "melody");
 		this.setInputsInline(false);
 		this.setPreviousStatement(true, null);
 		this.setNextStatement(true, null);
